25918851b9ad36b7ecaf36697ee570d096c3586b
Igor Afanasenko <igoraf07@gmail.com>
2016-11-25 11:00:56 +0200

    Fixed HammingDistance.

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
@@ -4,25 +4,17 @@ import java.util.Scanner;
 
 public class HammingDistance {
     public static int compute(String a, String a1) {
-        //System.out.println("Input first word");
-        //Scanner scanner = new Scanner(System.in);
-        //String a = scanner.nextLine();
 
-        //System.out.println("Input second word");
-        //Scanner scanner2 = new Scanner(System.in);
-        //String a1 = scanner2.nextLine();
-
-        int lena=a.length();
-        int lena1=a1.length();
-        int number=0; //Different
-        if (lena==lena1){
-            for (int i=0;i<lena;i++){
-            if (a.charAt(i)!=a1.charAt(i)){
-              number++;
+        int lengthA = a.length();
+        int lengthA1 = a1.length();
+        int number = 0; //Different
+        if (lengthA == lengthA1) {
+            for (int i = 0; i < lengthA; i++) {
+                if (a.charAt(i) != a1.charAt(i)) {
+                    number++;
+                }
             }
-            }
-        }
-        else {
+        } else {
             return -1;
 
         }
