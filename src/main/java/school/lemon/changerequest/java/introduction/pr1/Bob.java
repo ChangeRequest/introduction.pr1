2467acf358442937437dbfcfb7794d3f153dde9e
Anastasia <anabezrukavaya@gmail.com>
2016-11-26 11:54:42 +0200

    practise is done

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -4,7 +4,28 @@ package school.lemon.changerequest.java.introduction.pr1;
 public class Bob {
 
     public static String hey(String phrase) {
-        return "";
+        char ch;
+        int counter = 0;
+        boolean flag = true;
+        String reply = "";
+        if(phrase == "") return "Fine.";
+
+        for(int i = 0; i < phrase.length(); i++){
+            ch = phrase.charAt(i);
+            if(ch == '?'){
+                reply = "Yeap.";
+            } else if(ch == '!'){
+                reply = "Chill out!";
+            } else if(ch == '.') {
+                reply = "Whatever.";
+            } else if(ch == ' '){
+                counter++;
+            } else continue;
+        }
+        if(counter == phrase.length())
+            reply = "Fine.";
+
+        return reply;
     }
 
 }
--- src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
@@ -2,6 +2,16 @@ package school.lemon.changerequest.java.introduction.pr1;
 
 public class HammingDistance {
     public static int compute(String a, String a1) {
-        return -1;
+        int counter = 0;
+        if(a.length() != a1.length()) {
+            return -1;
+        } else {
+            for(int i = 0; i < a.length(); i++){
+                if(a.charAt(i) != a1.charAt(i)){
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
