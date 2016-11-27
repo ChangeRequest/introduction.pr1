9c38421f6e12e60bcb15faabb902501183703556
Ihor Kandaurov <heroy44@gmail.com>
2016-11-27 19:45:09 +0200

    fixes

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -1,18 +1,13 @@
 package school.lemon.changerequest.java.introduction.pr1;
 
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Bob {
 
     public static String hey(String phrase) {
-        Pattern pattern = Pattern.compile("[A-Z]");
-        Matcher matcher = pattern.matcher(phrase);
         if (phrase.trim().isEmpty()) {
             return "Fine.";
         }
-        if (phrase.toUpperCase().equals(phrase) && matcher.find()) {
+        if (phrase.equals(phrase.toUpperCase()) && !phrase.equals(phrase.toLowerCase())) {
             return "Chill out!";
         }
         if (phrase.endsWith("?")) {
--- src/main/java/school/lemon/changerequest/java/introduction/pr1/ConsoleCalculator.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/ConsoleCalculator.java
@@ -6,39 +6,49 @@ import java.util.Scanner;
 public class ConsoleCalculator {
 
     public static void main(String[] args) {
-        System.out.println("Make your choise: ");
-        System.out.println("Enter 'add' to perform addition.");
-        System.out.println("Enter 'sub' to perform subtraction.");
-        System.out.println("Enter 'mul' to perform multiplication.");
-        System.out.println("Enter 'div' to perform division.");
-        System.out.println("Enter 'exit' to exit.");
-        System.out.println("Enter 'help' to see help message.");
-        boolean eq = true;
-        while (eq) {
-            System.out.println("Make your choice.");
+        text();
+        boolean param = true;
+        while (param) {
             Scanner sc = new Scanner(System.in);
+            System.out.println("Make your choice.");
             String operation = sc.next();
             operation = operation.toLowerCase();
             switch (operation) {
                 case "add":
-                    addition();
+                    System.out.println("Enter first number:");
+                    int num1 = sc.nextInt();
+                    System.out.println("Enter second number:");
+                    int num2 = sc.nextInt();
+                    System.out.println("Result of " + num1 + "+" + num2 + " is " + (num1 + num2));
                     continue;
                 case "sub":
-                    subtraction();
+                    System.out.println("Enter first number:");
+                    num1 = sc.nextInt();
+                    System.out.println("Enter second number:");
+                    num2 = sc.nextInt();
+                    System.out.println("Result of " + num1 + "-" + num2 + " is " + (num1 - num2));
                     continue;
                 case "div":
-                    division();
+                    System.out.println("Enter first number:");
+                    num1 = sc.nextInt();
+                    System.out.println("Enter second number:");
+                    num2 = sc.nextInt();
+                    System.out.println("Result of " + num1 + "/" + num2 + " is " + (num1 / num2));
                     continue;
                 case "mul":
-                    multiplication();
+                    System.out.println("Enter first number:");
+                    num1 = sc.nextInt();
+                    System.out.println("Enter second number:");
+                    num2 = sc.nextInt();
+                    System.out.println("Result of " + num1 + "*" + num2 + " is " + (num1 * num2));
                     continue;
                 case "help":
-                    System.out.println("As example you need to type 'add' when you need 'Addition");
+                    text();
                     continue;
                 case "exit":
                     System.out.println("Bye-bye");
-                    eq = false;
                     sc.close();
+                    param = false;
                     break;
                 default:
                     System.out.println("Sorry, invalid choise.");
@@ -47,43 +57,13 @@ public class ConsoleCalculator {
         }
     }
 
-    private static void addition() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = sc.nextInt();
-        System.out.println("Enter second number:");
-        int num2 = sc.nextInt();
-        sc.close();
-        System.out.println("Result of " + num1 + "+" + num2 + " is " + (num1 + num2));
-    }
-
-    private static void subtraction() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = sc.nextInt();
-        System.out.println("Enter second number:");
-        int num2 = sc.nextInt();
-        sc.close();
-        System.out.println("Result of " + num1 + "-" + num2 + " is " + (num1 - num2));
-    }
-
-    private static void division() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = sc.nextInt();
-        System.out.println("Enter second number:");
-        int num2 = sc.nextInt();
-        sc.close();
-        System.out.println("Result of " + num1 + "/" + num2 + " is " + (num1 / num2));
-    }
-
-    private static void multiplication() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first number:");
-        int num1 = sc.nextInt();
-        System.out.println("Enter second number:");
-        int num2 = sc.nextInt();
-        sc.close();
-        System.out.println("Result of " + num1 + "*" + num2 + " is " + (num1 * num2));
+    private static void text() {
+        System.out.println("Make your choise: ");
+        System.out.println("Enter 'add' to perform addition.");
+        System.out.println("Enter 'sub' to perform subtraction.");
+        System.out.println("Enter 'mul' to perform multiplication.");
+        System.out.println("Enter 'div' to perform division.");
+        System.out.println("Enter 'exit' to exit.");
+        System.out.println("Enter 'help' to see help message.");
     }
 }
