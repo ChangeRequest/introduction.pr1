5f9456406ec03ac2fa126dc3f2e4ba2d2d53fdeb
Lera Kulikova <eklera0506@gmail.com>
2016-11-28 05:11:23 +0200

    Please help with regexp in Tasr Bob

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -1,10 +1,30 @@
 package school.lemon.changerequest.java.introduction.pr1;
 
 
+import java.util.Scanner;
+
 public class Bob {
 
     public static String hey(String phrase) {
-        return "";
+        if (phrase == null || phrase.trim().length() == 0) {
+            return "Fine";
+        }
+        if (phrase.matches("^[A-Z\\p{Lu}]+[\\d\\W]*$")) { // "^[A-Z]+[\\d\\W]*$"  ("^[A-Z\\d\\W]+$") - working[A-Z0-9_]+[\\w]*"))  A-Z \d\W]+$
+                return "Chill out!";
+        }
+        if (phrase.endsWith("?")) {
+                return "Yeap.";
+        }
+            return "Whatever.";
+        }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Speak to Bob!");
+        String speech = scanner.nextLine();
+        System.out.println(hey(speech));
+
     }
 
 }
--- src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
@@ -1,7 +1,20 @@
 package school.lemon.changerequest.java.introduction.pr1;
 
+
 public class HammingDistance {
     public static int compute(String a, String a1) {
-        return -1;
+        if (a.length() != a1.length()) return -1;
+        int count = 0;
+        for (int i = 0, j = 0; i < a.length(); i++, j++) {
+            if (a.charAt(i) != a1.charAt(j)) {
+                count++;
+            }
+        }
+        return count;
     }
+
+    public static void main(String[] args) {
+        System.out.println(compute("123", "1234"));
+    }
+
 }
