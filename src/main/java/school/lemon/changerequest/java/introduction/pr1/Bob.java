fe461b245b3334f399825c0ceeda881a4d28f99c
Tatyana Korchinova <tatyana.korchinova@gmail.com>
2016-11-28 18:26:23 +0400

    Tasks 1,2 are done.

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -4,7 +4,24 @@ package school.lemon.changerequest.java.introduction.pr1;
 public class Bob {
 
     public static String hey(String phrase) {
-        return "";
-    }
 
+
+        if (phrase.equals(phrase.toUpperCase()) && !phrase.equals(phrase.toLowerCase())) {
+            return "Chill out!";
+        }
+
+
+        if (phrase.endsWith("?")) {
+            return "Yeap.";
+        }
+
+
+
+        String trimmed = phrase.trim();
+        if (trimmed.isEmpty()) {
+            return "Fine.";
+        }
+                return "Whatever.";
+    }
 }
+
--- src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/HammingDistance.java
@@ -2,6 +2,18 @@ package school.lemon.changerequest.java.introduction.pr1;
 
 public class HammingDistance {
     public static int compute(String a, String a1) {
-        return -1;
+
+        if (a.length() != a1.length()) {
+            return -1;
+        }
+
+        int counter = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != a1.charAt(i)) {
+                counter++;
+            }
+        }
+
+        return counter;
     }
 }
