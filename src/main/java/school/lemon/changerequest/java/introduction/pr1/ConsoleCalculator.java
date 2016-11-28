d45703c659eaaea8c5ad77cdf39f4676bdb1c25e
Ihor Kandaurov <heroy44@gmail.com>
2016-11-28 16:17:40 +0200

    + added verification, deleted duplicatons of code

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/ConsoleCalculator.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/ConsoleCalculator.java
@@ -9,57 +9,50 @@ public class ConsoleCalculator {
         listOfChoises();
         Scanner sc = new Scanner(System.in);
         int num1, num2;
-        boolean param = true;
-        while (param) {
+        while (true) {
             System.out.println("Make your choice.");
-            String operation = sc.next();
-            operation = operation.toLowerCase();
+            String operation = sc.next().toLowerCase();
+            if (!operation.equals("exit, help, add, sub, div, mul")) {
+                System.out.println("Your type not ringt, you was trying - " + "/" + operation + "/" + " but bellow that you can do");
+                listOfChoises();
+                continue;
+            } else if (operation.equals("exit")) {
+                System.out.println("Bye-bye");
+                break;
+
+            } else if (operation.equals("help")) {
+                listOfChoises();
+                continue;
+            }
+            System.out.println("Enter first number:");
+            num1 = sc.nextInt();
+            System.out.println("Enter second number:");
+            num2 = sc.nextInt();
+            String resText = "Result of " + num1 + "+" + num2 + " is ";
             switch (operation) {
                 case "add":
-                    System.out.println("Enter first number:");
-                    num1 = sc.nextInt();
-                    System.out.println("Enter second number:");
-                    num2 = sc.nextInt();
-                    System.out.println("Result of " + num1 + "+" + num2 + " is " + (num1 + num2));
+                    System.out.println(resText + (num1 + num2));
                     continue;
                 case "sub":
-                    System.out.println("Enter first number:");
-                    num1 = sc.nextInt();
-                    System.out.println("Enter second number:");
-                    num2 = sc.nextInt();
-                    System.out.println("Result of " + num1 + "-" + num2 + " is " + (num1 - num2));
+                    System.out.println(resText + (num1 - num2));
                     continue;
                 case "div":
-                    System.out.println("Enter first number:");
-                    num1 = sc.nextInt();
-                    System.out.println("Enter second number:");
-                    num2 = sc.nextInt();
-                    System.out.println("Result of " + num1 + "/" + num2 + " is " + (num1 / num2));
+                    System.out.println(resText + (num1 / num2));
                     continue;
                 case "mul":
-                    System.out.println("Enter first number:");
-                    num1 = sc.nextInt();
-                    System.out.println("Enter second number:");
-                    num2 = sc.nextInt();
-                    System.out.println("Result of " + num1 + "*" + num2 + " is " + (num1 * num2));
+                    System.out.println(resText + (num1 * num2));
                     continue;
-                case "help":
-                    listOfChoises();
-                    continue;
-                case "exit":
-                    System.out.println("Bye-bye");
-                    sc.close();
-                    param = false;
-                    break;
                 default:
                     System.out.println("Sorry, invalid choise.");
 
             }
         }
+
     }
 
+
     private static void listOfChoises() {
-        System.out.println("Make your choise: ");
+        System.out.println("List of actions: ");
         System.out.println("Enter 'add' to perform addition.");
         System.out.println("Enter 'sub' to perform subtraction.");
         System.out.println("Enter 'mul' to perform multiplication.");
