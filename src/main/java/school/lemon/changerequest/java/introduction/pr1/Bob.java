d8f383c0217c8afdd9126432b752d2413b8fbfbe
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-12-02 05:17:29 +0200

    1 not all (with some problem) (UpperCase numbers)

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -7,23 +7,20 @@ import java.util.regex.Pattern;
 public class Bob {
 
     public static String hey(String phrase) {
-        String tabul = "\\s";
-        Pattern patternPharse = Pattern.compile(tabul);
-        Matcher matcherPharse = patternPharse.matcher(phrase);
-        if (phrase.equals(matcherPharse)) {
+
+
+        if (phrase.equals(phrase.toUpperCase()) ) {
+            return "Chill out!";
+        }
+     else   if (phrase.endsWith("?")) {
+            return "Yeap.";
+        }
+        String trimmed = phrase.trim();
+        if (trimmed.isEmpty()) {
             return "Fine.";
         }
 
-      else if (phrase.equals(phrase.toUpperCase())) {
-        return "Chill out!";
-    }
-
-      else  if (phrase.endsWith("?")) {
-            return "Yeap.";
-        }
-
-
-         else return "Whatever.";
+        return "Whatever.";
     }
 
 }
