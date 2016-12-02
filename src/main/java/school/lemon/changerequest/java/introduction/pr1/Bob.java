afb102d66ab297da6abaa254790934a748cf238a
dianasinenchenko <diana.sinenchenko@gmail.com>
2016-12-02 04:14:24 +0200

    1 not all (without "Fine")

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -1,10 +1,29 @@
 package school.lemon.changerequest.java.introduction.pr1;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Bob {
 
     public static String hey(String phrase) {
-        return "";
+        String tabul = "\\s";
+        Pattern patternPharse = Pattern.compile(tabul);
+        Matcher matcherPharse = patternPharse.matcher(phrase);
+        if (phrase.equals(matcherPharse)) {
+            return "Fine.";
+        }
+
+      else if (phrase.equals(phrase.toUpperCase())) {
+        return "Chill out!";
+    }
+
+      else  if (phrase.endsWith("?")) {
+            return "Yeap.";
+        }
+
+
+         else return "Whatever.";
     }
 
 }
