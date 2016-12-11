b5eaed2c14f3654ef48e15f56e603d9a48f956aa
Olga Berezhna <berezhna.o.a@gmail.com>
2016-12-11 23:31:07 +0200

    Bob and HD done

--- src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
+++ src/main/java/school/lemon/changerequest/java/introduction/pr1/Bob.java
@@ -4,7 +4,19 @@ package school.lemon.changerequest.java.introduction.pr1;
 public class Bob {
 
     public static String hey(String phrase) {
-        return "";
+
+        if (phrase.trim().isEmpty()) {
+            return "Fine.";
+        }
+
+        if (phrase.equals(phrase.toUpperCase()) && !phrase.equals(phrase.toLowerCase())) {
+            return "Chill out!";
+        }
+
+        if (phrase.endsWith("?")) {
+            return "Yeap.";
+        }
+        else return "Whatever.";
     }
 
 }
